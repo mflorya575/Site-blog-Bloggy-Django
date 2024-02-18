7e977b3879f20c0966860189574a7bf54d3d2267
Michael Florya <mih2303@mail.ru>
2024-02-18 20:43:25 +0300

    django-mptt, django-mptt-admin, были переделаны модели (созданы древовидные модели категорий)

--- blog_cbv/apps/blog/models.py
+++ blog_cbv/apps/blog/models.py
@@ -1,6 +1,43 @@
 from django.db import models
-from django.core.validators import FileExtensionValidator
-from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    """
+    Модель категорий с вложенностью
+    """
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(max_length=255, verbose_name='URL категории', blank=True)
+    description = models.TextField(verbose_name='Описание категории', max_length=300)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        db_index=True,
+        related_name='children',
+        verbose_name='Родительская категория'
+    )
+
+    class MPTTMeta:
+        """
+        Сортировка по вложенности
+        """
+        order_insertion_by = ('title',)
+
+    class Meta:
+        """
+        Сортировка, название модели в админ панели, таблица в данными
+        """
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        db_table = 'app_categories'
+
+    def __str__(self):
+        """
+        Возвращение заголовка статьи
+        """
+        return self.title
 
 
 class Post(models.Model):
@@ -17,29 +54,10 @@ class Post(models.Model):
     slug = models.SlugField(verbose_name='URL', max_length=255, blank=True, unique=True)
     description = models.TextField(verbose_name='Краткое описание', max_length=500)
     text = models.TextField(verbose_name='Полный текст записи')
-    thumbnail = models.ImageField(default='default.jpg',
-        verbose_name='Изображение записи',
-        blank=True,
-        upload_to='images/thumbnails/',
-        validators=[FileExtensionValidator(allowed_extensions=('png', 'jpg', 'webp', 'jpeg', 'gif'))]
-    )
-    status = models.CharField(choices=STATUS_OPTIONS, default='published', verbose_name='Статус записи', max_length=10)
-    create = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления')
-    update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
-    author = models.ForeignKey(to=User, verbose_name='Автор', on_delete=models.SET_DEFAULT, related_name='author_posts',
-                               default=1)
-    updater = models.ForeignKey(to=User, verbose_name='Обновил', on_delete=models.SET_NULL, null=True,
-                                related_name='updater_posts', blank=True)
-    fixed = models.BooleanField(verbose_name='Прикреплено', default=False)
+    category = TreeForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
 
     class Meta:
-        db_table = 'blog_post'
-        ordering = ['-fixed', '-create']
-        indexes = [models.Index(fields=['-fixed', '-create', 'status'])]
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
-
-    def __str__(self):
-        return self.title
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
 
 # Create your models here.
--- blog_cbv/blog_cbv/settings.py
+++ blog_cbv/blog_cbv/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.blog.apps.BlogConfig',
+    'mptt',
+    'django_mptt_admin',
 ]
 
 MIDDLEWARE = [
