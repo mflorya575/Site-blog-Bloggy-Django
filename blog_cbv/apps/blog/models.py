6128ae5423af0b3382dbf7e9388b01e5c8e12706
Michael Florya <mih2303@mail.ru>
2024-02-20 01:18:07 +0300

    добавлен bootstrap, добавлены файлы html и стилизация, пагинация

--- blog_cbv/apps/blog/models.py
+++ blog_cbv/apps/blog/models.py
@@ -38,6 +38,12 @@ class Category(MPTTModel):
         verbose_name_plural = 'Категории'
         db_table = 'app_categories'
 
+    def get_absolute_url(self):
+        """
+        Получаем прямую ссылку на категорию
+        """
+        return reverse('post_by_category', kwargs={'slug': self.slug})
+
     def __str__(self):
         """
         Возвращение заголовка статьи
