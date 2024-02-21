0b6804cb0f63df430e2e62c90c1e5153244023f1
Michael Florya <mih2303@mail.ru>
2024-02-21 20:50:37 +0300

    добавлены шаблоны комментариев, стилизован шаблон post_detail, написан js код для комментариев

--- blog_cbv/blog_cbv/settings.py
+++ blog_cbv/blog_cbv/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = (BASE_DIR / 'static')
+STATICFILES_DIRS = [BASE_DIR / 'templates/js/']
 
 MEDIA_ROOT = (BASE_DIR / 'media')
 MEDIA_URL = '/media/'
