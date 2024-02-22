ab4f5557fca870210a6cae07031dabacce14a20f
Michael Florya <mih2303@mail.ru>
2024-02-22 15:49:14 +0300

    добавлена RSS лента

--- blog_cbv/blog_cbv/urls.py
+++ blog_cbv/blog_cbv/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.blog.feeds import LatestPostFeed
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.blog.urls')),
+    path('feeds/latest/', LatestPostFeed(), name='latest_post_feed'),
     path('', include('apps.accounts.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
