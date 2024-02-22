3e0d41226ece4817c006fc4b12d678e3623a933f
Michael Florya <mih2303@mail.ru>
2024-02-22 19:06:42 +0300

    настройка шаблонов для страниц ошибок 403, 404, 500

--- blog_cbv/apps/blog/views.py
+++ blog_cbv/apps/blog/views.py
@@ -181,4 +181,34 @@ class RatingCreateView(View):
                 return JsonResponse({'status': 'updated', 'rating_sum': rating.post.get_sum_rating()})
         return JsonResponse({'status': 'created', 'rating_sum': rating.post.get_sum_rating()})
 
+
+def tr_handler404(request, exception):
+    """
+    Обработка ошибки 404
+    """
+    return render(request=request, template_name='errors/error_page.html', status=404, context={
+        'title': 'Страница не найдена: 404',
+        'error_message': 'К сожалению такая страница была не найдена, или перемещена',
+    })
+
+
+def tr_handler500(request):
+    """
+    Обработка ошибки 500
+    """
+    return render(request=request, template_name='errors/error_page.html', status=500, context={
+        'title': 'Ошибка сервера: 500',
+        'error_message': 'Внутренняя ошибка сайта, вернитесь на главную страницу, отчет об ошибке мы направим администрации сайта',
+    })
+
+
+def tr_handler403(request, exception):
+    """
+    Обработка ошибки 403
+    """
+    return render(request=request, template_name='errors/error_page.html', status=403, context={
+        'title': 'Ошибка доступа: 403',
+        'error_message': 'Доступ к этой странице ограничен',
+    })
+
 # Create your views here.
