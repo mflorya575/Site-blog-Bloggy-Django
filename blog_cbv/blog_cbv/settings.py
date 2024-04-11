1238fd8ee57997490b65b2c18033f2ddbcd9fecb
Michael Florya <mih2303@mail.ru>
2024-04-11 19:55:58 +0300

    change database to postgresql

--- blog_cbv/blog_cbv/settings.py
+++ blog_cbv/blog_cbv/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'blog_cbv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bloggy',
+        'USER': 'bloggy',
+        'PASSWORD': '54321',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
